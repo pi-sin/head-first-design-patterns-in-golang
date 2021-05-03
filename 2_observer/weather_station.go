ee8ed9e8f38f21dc9f0ddfe328c36d8e70be08d7
Piyush SInha <piyush.s@pocket52.com>
2021-05-04 02:41:40 +0530

    Commenting Code

--- 2_observer/weather_station.go
+++ 2_observer/weather_station.go
@@ -1,8 +1,15 @@
 package main
 
 func main() {
+	/**
+	 * Creating the weather object
+	 */
 	weatherData := newWeatherData()
 
+	/**
+	 * Creating the displays and registering as
+	 * observer to the Subject
+	 */
 	currentConditionDisplay := newCurrentConditionDisplay()
 	weatherData.registerObserver(currentConditionDisplay)
 
@@ -11,10 +18,17 @@ func main() {
 	//forecastDisplay := newForecastDisplay()
 	//weatherData.registerObserver(forecastDisplay)
 
+	/**
+	 * Simulate new weather measurements
+	 */
 	weatherData.setMeasurements(80, 65, 30.4)
 	weatherData.setMeasurements(82, 70, 29.2)
 	weatherData.setMeasurements(77, 90, 29.2)
 
+	/**
+	 * Deregistering the statisticsDisplay
+	 * Change in measurements won't notify this observer
+	 */
 	weatherData.deregisterObserver(statisticsDisplay)
 
 	weatherData.setMeasurements(84, 80, 32.6)
@@ -22,5 +36,4 @@ func main() {
 	weatherData.registerObserver(statisticsDisplay)
 
 	weatherData.setMeasurements(84, 80, 32.6)
-
 }
